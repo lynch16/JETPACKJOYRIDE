54dee4209f0dd1060c065bc7ac546003ef68819e
Will Lynch <will.lynch91@gmail.com>
2025-10-03 11:37:45 -0400

    Spawn coins and enemeis

--- Models/SpawnManager.cs
+++ Models/SpawnManager.cs
@@ -6,7 +6,11 @@ public partial class SpawnManager : Node
 {
     [Export]
     private PackedScene[] EnemyScenes { get; set; }
+    [Export]
+    private PackedScene CoinScene { get; set; }
     private PackedScene _nextSpawn { get; set; }
+
+    private int _coinRatio = 100/20; // Spawn coins 20% of the time
 
     public void OnStart()
     {
@@ -21,7 +25,20 @@ public partial class SpawnManager : Node
     private void StartSpawnTimer()
     {
         var _newEnemyType = GD.Randi() % EnemyScenes.Length;
-        _nextSpawn = EnemyScenes[_newEnemyType];
+
+        // Generate random number between 0-100. Spawn coin if multiple of 10;
+        var random = (GD.Randi() % 100);
+        var shouldSpawnCoin = random % _coinRatio == 0;
+
+        if (shouldSpawnCoin)
+        {
+            _nextSpawn = CoinScene;
+        }
+        else
+        {
+            _nextSpawn = EnemyScenes[_newEnemyType];
+        }
+
         GetNode<Timer>("SpawnTimer").Start();
     }
 
